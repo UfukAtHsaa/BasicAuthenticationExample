6a159c8f21f29aed2ec6ebdd72329fffd3ee028b
de313e <ufuk.altin@kiongroup.com>
2022-11-08 10:49:58 +0100

    Added Basic Auth with Login

---
+++ src/test/java/de/hsaa/springbasicauthbackend/SpringBasicAuthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsaa.springbasicauthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
